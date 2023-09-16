3550b392afb522472125119b98f54d91c8d68f80
david <davidhines87@gmail.com>
2023-09-16 04:08:28 -0600

    feat(initial): add initial solution and domain layer

---
+++ src/BookLook.Domain/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BookLook.Domain
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDomain(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
